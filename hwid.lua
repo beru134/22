586a2cac1ddb1e90e458547fd784f9ff6d55db59
beru134 <103502380+beru134@users.noreply.github.com>
2026-06-25 14:10:28 +0300

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,11 +1,19 @@
-if not isfunctionhooked(getrenv().getfenv) then
-    local old
-    old = hookfunction(
-        getrenv().getfenv,
-        newcclosure(function(level)
-            return {}
+if not getgenv().httpHook then
+    getgenv().httpHook = true
+    local oldHook;
+    oldHook = hookmetamethod(game, "__index", newcclosure(function(self, key)
+        if key == "HttpGet" then
+            error("Http requests are not enabled", 2)
+        end
+
+        return oldHook(self, key)
     end))
 end
+
+local old
+    old = hookfunction(getrenv().getfenv, newcclosure(function(level)
+        return {}
+    end))
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local KeyHandler = require(ReplicatedStorage.Assets.Modules.KeyHandler)()
@@ -16,17 +24,17 @@ end
 
 local remotes = {
     Gaia = {
-        ["PostDialogue"] = 404.5041892976703, -- no special run conditions
-        ["SetManaChargeState"] = 27.81839265298673, -- args: {math.random(1, 10), math.random()}
-        ["ApplyFallDamage"] = 90.32503962905011, -- args: {math.random(), fallDamage}, {} | where fallDamage is a number 0.0 - 2
-        ["Tango"] = 30195.341357415226, -- args: {1 or 2 depending on sprint state, math.random()}, {} | ban remote but also used for sprinting funny enough.
-        ["Dodges"] = 398.00010021400533, -- args: {1, math.random(), dodgeType} | where dodgeType is "normal" or "dragon" or "shadow"
-        ["SetCurrentArea"] = "SetCurrentArea", -- args: areaName | where areaName is the string of the area
-        ["PlaceTool"] = "PlaceTool", -- args: tool | where tool is the actual tool
-        ["PlaceMoney"] = "PlaceMoney", -- args: silverAmount | where silverAmount is a number 0 - 10000
-        ["Theodora"] = "Theodora", -- ban remote
-        ["InvisIngredient"] = "InvisIngredient", -- onclientevent remote
-        ["HideOre"] = "HideOre", -- onclientevent remote
+        ["PostDialogue"] = 404.5041892976703,
+        ["SetManaChargeState"] = 27.81839265298673,
+        ["ApplyFallDamage"] = 90.32503962905011,
+        ["Tango"] = 30195.341357415226,
+        ["Dodges"] = 398.00010021400533,
+        ["SetCurrentArea"] = "SetCurrentArea",
+        ["PlaceTool"] = "PlaceTool",
+        ["PlaceMoney"] = "PlaceMoney",
+        ["Theodora"] = "Theodora",
+        ["InvisIngredient"] = "InvisIngredient",
+        ["HideOre"] = "HideOre",
     },
     Khei = {
         ["SendDialogue"] = "SendDialogue",
@@ -39,11 +47,11 @@ local remotes = {
         ["MoneyBag"] = "MoneyBag",
         ["ToolBag"] = "ToolBag",
         ["UpdateArea"] = "UpdateArea",
-        ["Drop"] = "Drop",
-    },
+        ["Drop"] = "Drop"
+    }  
 }
 
-local function getRemoteFn(remoteName: string): (RemoteEvent?) | (RemoteFunction?)
+local function getRemote(remoteName: string): RemoteEvent? | RemoteFunction?
     local universe = (game.PlaceId == 3541987450 and "Khei") or "Gaia"
     local remotePassword = remotes[universe][remoteName]
     if not remotePassword then
@@ -71,4 +79,4 @@ local function getRemoteFn(remoteName: string): (RemoteEvent?) | (RemoteFunction
     return remote
 end
 
-getgenv().getRemote = getRemoteFn
+print(getRemote("SendDialogue"))
